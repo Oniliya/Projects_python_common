49989dd8f21975c7365dbd9d22f12d04e35eb682
Oni <oni-mail@mail.ru>
2023-05-03 23:23:39 +0300

    03_05_2023_2

--- Dirty_Python_Basic/hw2_7_dp_add.py
+++ Dirty_Python_Basic/hw2_7_dp_add.py
@@ -3,21 +3,58 @@
 # 2. На шахматной доске расположить 8 ферзей так, чтобы они не били друг друга
 # приложить хотя бы один вариант такой расстановки
 
-def put_queen():
-    return None
+def put_queen(num_i, num_j :int ,my_list: list) ->list:
+    for i in range(8):
+        for j in range(8):
+            if i== num_i or j== num_j:
+                my_list[i][j]=1
+            if j-num_j==i-num_i:
+                my_list[i][j]=1
+            if i-num_i==num_j-j:
+                my_list[i][j]=1
+            if i== num_i and j== num_j:
+                my_list[i][j]=8
+    return my_list
 
+def print_board(my_board: list):
+    for item in my_board:
+        print(item)
+
+def clear_board(my_board: list) -> list:
+    new_board=[]
+    for i in range(8):
+        new_board.insert(i,[0,0,0,0,0,0,0,0])
+    return new_board
 
 def main():
     board=[]
     for i in range(8):
         board.insert(i,[0,0,0,0,0,0,0,0])
-        print(board[i])
+    print_board(board)
 
-    put_queen(i,j)
+    for i in range(8):
+        for j in range(8):
+            if board[i][j]!=8 and board[i][j]!=1:
+                put_queen(i,j,board)
+    
+    print('------------------------')
+    print_board(board)
+
+
+    # put_queen(5-1,5-1,board)
+    # print('------------------------')
+    # print_board(board)
 
 main()
 
 
+
+
+# my_list=[1,5,6,4,9,7]
+# print(my_list)
+
+# my_list[2]=190
+# print(my_list)
 
 
 
