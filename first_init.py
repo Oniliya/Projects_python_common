5dd9f6174f1d208660b159ab364034a473aa51c6
Oni <oni-mail@mail.ru>
2023-06-03 21:09:05 +0300

    03_06_2023

--- first_init.py
+++ first_init.py
@@ -1,9 +1,28 @@
-print("hello")
-print("hello")
+# print("hello")
+# print("hello")
 
-print("hello world")
+# print("hello world")
 
-git init
-git status
-git add ....
-git push
+# git init
+# git status
+# git add ....
+# git push
+
+
+myfile = '''Sykes;26;31;76
+Carroll;26;78;82
+Bawerman;85;89;98'''
+s = 0
+a = [0, 0, 0]
+student = myfile.split(';')
+print(student)
+
+# for student in myfile:
+    
+#     s = (int(student[1]) + int(student[2]) + int(student[3])) / 3
+#     a[0] += int(student[1])
+#     a[1] += int(student[2])
+#     a[2] += int(student[3])
+#     print(str(s) + '\n')
+# for i in a:
+#     print(str(i / len(myfile)) + ' ')
